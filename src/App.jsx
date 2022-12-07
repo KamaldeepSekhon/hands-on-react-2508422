44f37db0cd973e0487e962aefa686c2a51acb3c0
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-12-06 18:39:26 -0800

    lec on hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import React, { component } from "react";
+import Welcome from "./components/Welcome"; //import file Welcome.jsx
+// class Welcome extends React.Component {
+//   constructor() {
+//     super();
+//   }
+//   render() {
+//     return <h1>First React Project</h1>;
+//   }
+// }
 
-class Welcome extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return <h1>First React Project</h1>;
-  }
-}
+// class Welcome2 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return <h1>{this.props.name} React Project</h1>;
+//   }
+// }
 
-class Welcome2 extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <h1>{this.props.name} React Project</h1>;
-  }
-}
-
-const Welcome3 = (props) => {
-  //hooks
-  return (
-    <h1>
-      {" "}
-      Hello <i>{props.gold}</i>
-    </h1>
-  );
-};
+// const Welcome3 = (props) => {
+//   //hooks
+//   return (
+//     <h1>
+//       {" "}
+//       Hello <i>{props.gold}</i>
+//     </h1>
+//   );
+// };
 function App() {
   const name = "StarGazers";
   return (
@@ -34,10 +34,12 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome />
+          <Welcome name="First Project"></Welcome>
+          {/* <Welcome />
           <Welcome2 name="second" />
           <Welcome2 name="third" />
-          <Welcome3 gold="Kamal" />
+          <Welcome3 gold="Kamal" /> */}
+
           <h1>
             Meet the <i style={{ color: "SteelBlue" }}>{name}</i>
           </h1>
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,3 @@
+export default (props)=>{
+  return <h1>Welcome to the React {props.name}</h1>
+}
