4ff6bf4722a5fe61fb982922f888a5e5d88985c7
Kamaldeep Sekhon <kksekhon4@gmail.com>
2022-12-06 17:51:02 -0800

    lec class and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import ListCast from './components/ListCast';
-import Modals from './components/Modals';
-import Nav from './components/Nav';
-import "./App.scss";
+import React, { component } from "react";
 
-function App() {
-  const [cast, setCast] = useState([]);
-  let [memberInfo, setMemberInfo] = useState(null);
-
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
+class Welcome extends React.Component {
+  constructor() {
+    super();
   }
+  render() {
+    return <h1>First React Project</h1>;
+  }
+}
 
-  useEffect(() => {
-    fetchCast();
-  });
+class Welcome2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>{this.props.name} React Project</h1>;
+  }
+}
 
+const Welcome3 = (props) => {
+  //hooks
   return (
-    <>
-      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-      <div className="container">
+    <h1>
+      {" "}
+      Hello <i>{props.gold}</i>
+    </h1>
+  );
+};
+function App() {
+  const name = "StarGazers";
+  return (
+    <div className="container">
+      <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the Stargazers</h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-          {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
+          <Welcome />
+          <Welcome2 name="second" />
+          <Welcome2 name="third" />
+          <Welcome3 gold="Kamal" />
+          <h1>
+            Meet the <i style={{ color: "SteelBlue" }}>{name}</i>
+          </h1>
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <button className="outline" onClick={() => alert("Hi there")}>
+            Click Me
+          </button>
         </hgroup>
-      </div>
-    </>
-  )
+      </article>
+    </div>
+  );
 }
-export default App
+export default App;
